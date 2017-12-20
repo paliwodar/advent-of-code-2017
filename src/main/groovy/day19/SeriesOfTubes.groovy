d4a5c703e50da1c3294d1d26b54284cbbaece69d
Rafal Paliwoda <rp@message.pl>
2017-12-20 17:39:21 +0100

    day 19

--- src/main/groovy/day19/SeriesOfTubes.groovy
+++ src/main/groovy/day19/SeriesOfTubes.groovy
@@ -6,32 +6,31 @@ class SeriesOfTubes {
 
     static Tuple2<String, Integer> calc(List<String> input) {
         int start = input[0].indexOf('|')
-        return go(input, start, 0, 0, 1, "", 0)
+        return go(input, start, 0, new Tuple2<Integer, Integer>(0, 1), "", 0)
     }
 
     @TailRecursive
-    static Tuple2<String, Integer> go(List<String> strings, int startX, int startY, int dirX, int dirY, String acc, int counter) {
+    static Tuple2<String, Integer> go(List<String> pipes, int currentX, int currentY, Tuple2<Integer, Integer> direction, String acc, int counter) {
 
-        char current = strings[startY][startX]
+        char current = pipes[currentY][currentX]
 
         if (current == " ") {
             return new Tuple2<String, Integer>(acc, counter)
-        } else if (current == "+") {
-            Tuple2<Integer, Integer> newDir = getNewDirection(strings, startX, startY, dirX, dirY)
-            return go(strings, startX + newDir.first, startY + newDir.second, newDir.first, newDir.second, acc, counter + 1)
         } else {
-            return go(strings, startX + dirX, startY + dirY, dirX, dirY, acc + (current.isLetter() ? current : ""), counter + 1)
+            final
+            def newDirection = (current == "+" ? getNewDirection(pipes, currentX, currentY, direction) : direction)
+            return go(pipes, currentX + newDirection.first, currentY + newDirection.second, newDirection, acc + (current.isLetter() ? current : ""), counter + 1)
         }
     }
 
-    static Tuple2<Integer, Integer> getNewDirection(List<String> strings, int startX, int startY, int dirX, int dirY) {
-        if (dirX == 0) {
+    static Tuple2<Integer, Integer> getNewDirection(List<String> strings, int startX, int startY, Tuple2<Integer, Integer> direction) {
+        if (direction.first == 0) {
             if (startX - 1 > 0 && strings.size() && strings[startY][startX - 1] != " ") {
                 return new Tuple2<Integer, Integer>(-1, 0)
             } else {
                 return new Tuple2<Integer, Integer>(1, 0)
             }
-        } else if (dirY == 0) {
+        } else if (direction.second == 0) {
             if (startY + 1 < strings.size() && strings[startY + 1][startX] != " ") {
                 return new Tuple2<Integer, Integer>(0, 1)
             } else {
